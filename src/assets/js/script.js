67fb75228dc4467b84150498f2beba1688aa2853
Erick Fellipe <efrc22br@gmail.com>
2025-11-06 16:26:46 -0300

    Finalizada programação do quiz

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -11,11 +11,73 @@ const question = document.querySelector(".question");
 const answersContainer = document.querySelector(".answers-container");
 const nextButton = document.getElementById("nextQuestionBtn");
 
+const congratulations = document.querySelector(".congratulations");
+const emojiContainer = document.querySelector(".emoji-container");
+const scoreMark = document.querySelector(".score-mark");
+
 let index = 0;
 let score = 0;
 let answered = false;
 let selectedBtn = null;
 let correctAnswer = null;
+
+const emojis = [
+  {
+    minScore: 0,
+    img: "src/assets/imgs/icons/x-mark.svg",
+    alt: "Imagem de uma marcação de X",
+  },
+  {
+    minScore: 1,
+    img: "src/assets/imgs/icons/crying.svg",
+    alt: "Imagem de um emoji chorando",
+  },
+  {
+    minScore: 2,
+    img: "src/assets/imgs/icons/sad.svg",
+    alt: "Imagem de um emoji triste",
+  },
+  {
+    minScore: 3,
+    img: "src/assets/imgs/icons/neutral-face.svg",
+    alt: "Imagem de um emoji com expressão neutra",
+  },
+  {
+    minScore: 4,
+    img: "src/assets/imgs/icons/confused.svg",
+    alt: "Imagem de um emoji confuso",
+  },
+  {
+    minScore: 5,
+    img: "src/assets/imgs/icons/smiling.svg",
+    alt: "Imagem de um emoji sorrindo",
+  },
+  {
+    minScore: 6,
+    img: "src/assets/imgs/icons/sunglasses.svg",
+    alt: "Imagem de um emoji com óculos escuros",
+  },
+  {
+    minScore: 7,
+    img: "src/assets/imgs/icons/star-eyed.svg",
+    alt: "Imagem de um emoji com os olhos estrelados",
+  },
+  {
+    minScore: 8,
+    img: "src/assets/imgs/icons/bronze-medal.svg",
+    alt: "Imagem de um emoji de medalha de bronze",
+  },
+  {
+    minScore: 9,
+    img: "src/assets/imgs/icons/silver-medal.svg",
+    alt: "Imagem de um emoji de medalha de prata",
+  },
+  {
+    minScore: 10,
+    img: "src/assets/imgs/icons/crown.svg",
+    alt: "Imagem de um emoji de coroa",
+  },
+];
 
 async function startQuiz() {
   index = 0;
@@ -127,6 +189,7 @@ async function fillQuestions(data, subject) {
       if (userChoice === correctAnswer) {
         selectedBtn.classList.remove("active");
         selectedBtn.classList.add("correct");
+        increaseScore();
       } else {
         selectedBtn.classList.remove("active");
         selectedBtn.classList.add("wrong");
@@ -159,6 +222,7 @@ function showNextQuestion(data, subject) {
   if (index >= data.materias[subject].perguntas.length - 1) {
     quiz.style.display = "none";
     scoreContainer.style.display = "flex";
+    updateScoreContainer();
     return;
   }
 
@@ -185,9 +249,53 @@ function increaseScore() {
   score++;
 }
 
+function updateScoreContainer() {
+  emojiContainer.innerHTML = "";
+
+  let congratulationsMsg = "";
+  let msg = "";
+
+  if (score < 1) {
+    msg = "Não fique decepcionado! Estude mais!";
+  } else if (score < 3) {
+    msg = "Continue treinando mais!";
+  } else if (score < 5) {
+    congratulationsMsg = "Puxa!";
+    msg = "Você está melhorando!";
+  } else if (score <= 7) {
+    congratulationsMsg = "Boa!";
+    msg = "Bom demais! Continue assim!";
+  } else if (score <= 9) {
+    congratulationsMsg = "Parabéns";
+    msg = "Você foi muito bem! Que incrível!";
+  } else {
+    congratulationsMsg = "ESPETACULAR!";
+    msg = "Você é de fato o REI DO QUIZ!";
+  }
+
+  let image = document.createElement("img");
+
+  for (let i = 0; i < emojis.length; i++) {
+    const emoji = emojis[i];
+
+    if (score === emoji.minScore) {
+      image.src = emoji.img;
+      image.alt = emoji.alt;
+      break;
+    }
+  }
+
+  congratulations.innerHTML = `<h2>${congratulationsMsg}</h2>`;
+  emojiContainer.appendChild(image);
+  scoreMark.innerHTML = `<p>Você acertou <span class="mark">${score}/10</span> questões do quiz!<p>
+                         <p>${msg}</p>`;
+}
+
 function exit() {
   scoreContainer.style.display = "none";
   startQuizContainer.style.display = "flex";
+
+  select.value = "";
 }
 
 function showErrorMsg(msg, element) {
